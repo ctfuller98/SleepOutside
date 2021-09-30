415ff66a0bbeb7f632d26c6abf2c15a775dcc118
Camthack13 <camronthackeray@gmail.com>
2021-09-30 10:03:25 -0600

    please work

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,27 @@
+import ProductData from './productData.js';
+import ProductDetails from './productDetails.js';
+import { getParam } from './utils.js';
+
+const dataSource = new ProductData('tents');
+const productId = getParam('product');
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+/*
+
 let products = [];
+
+// add to cart button event handler
+function addToCart(e) {
+  const product = products.find((item) => item.Id === e.target.dataset.id);
+  setLocalStorage('so-cart', product);
+}
+
+getData();
+// add listener to Add to Cart button
+document.getElementById('addToCart').addEventListener('click', addToCart);
+/*
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -23,13 +46,4 @@ function getProductsData() {
 // async function getProductsDataAwait() {
 //   products = await fetch("../json/tents.json").then(convertToJson);
 // }
-
-// add to cart button event handler
-function addToCart(e) {
-  const product = products.find((item) => item.Id === e.target.dataset.id);
-  setLocalStorage('so-cart', product);
-}
-
-getProductsData();
-// add listener to Add to Cart button
-document.getElementById('addToCart').addEventListener('click', addToCart);
+*/
--- src/js/utils.js
+++ src/js/utils.js
@@ -21,3 +21,11 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener('click', callback);
 }
+// gets parameters for products details
+export function getParam(param){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get('product');
+  console.log(product);
+  return product;
+} 
